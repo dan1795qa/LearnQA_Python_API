ca427437817cd91692bff294c01175b2ea776abf
unknown <dan1795dan@gmail.com>
2023-11-19 00:35:36 +0300

    HW3

--- Request API/2.headers.py
+++ Request API/2.headers.py
@@ -1,7 +1,7 @@
 import requests
 
 headers = {"some_headers": "123"}
-response = requests.get("https://playground.learnqa.ru/api/show_all_headers", headers=headers)
+response = requests.get("https://playground.learnqa.ru/api/show_all_headers",)
 
-print(response.text)    # ЗАГОЛОВКИ ЗАПРОСА
-print(response.headers)    # ЗАГОЛОВКИ ОТВЕТА
+print(response.text)    # ОТВЕТ СЕРВЕРА
+print(response.headers)    # ЗАГОЛОВКИ ОТВЕТА СЕРВЕРА НА ЭТОТ ЗАПРОС
