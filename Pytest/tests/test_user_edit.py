71c7c8f0d89f7028963d08873d8fe1de0a90b003
unknown <dan1795dan@gmail.com>
2023-11-25 16:19:15 +0300

    test_user_edit

---
+++ Pytest/tests/test_user_edit.py
@@ -0,0 +1,66 @@
+import requests
+from Pytest.lib.base_case import BaseCase
+from Pytest.lib.assertions import Assertions
+
+
+class TestUserEdit(BaseCase):
+
+    def test_edit_just_created_user(self):
+
+        """"REGISTER"""""
+
+        register_data = self.prepare_registration_data()
+        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        first_name = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+        print(user_id)
+
+
+        """"LOGIN"""
+
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        print(auth_sid)
+        token = self.get_header(response2, "x-csrf-token")
+        print(token)
+
+        """"EDIT"""
+
+        new_name = "Changed_Name"
+
+        response3 = requests.put(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid},
+                                 data={"first_name": new_name}
+                                 )
+
+        Assertions.assert_code_status(response3, 200)
+
+
+        """"GET"""
+
+        response4 = requests.get(f"https://playground.learnqa.ru/api/user/{user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid}
+                                 )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "username",
+            new_name,
+            "Wrong name of the user after edit"
+        )
+
+
