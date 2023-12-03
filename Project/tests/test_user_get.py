e62643c5ca6165e19fd5d346b93ecfd7f2400507
unknown <dan1795dan@gmail.com>
2023-12-03 15:07:28 +0300

    HW done: 'This test get details another user'

--- Project/tests/test_user_get.py
+++ Project/tests/test_user_get.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 import allure
-
 from Project.lib.my_requests import MyRequests
 import requests
 from Project.lib.base_case import BaseCase
@@ -16,6 +14,7 @@ class TestUserGet(BaseCase):
     def test_get_user_details_not_auth(self):
 
         response = MyRequests.get("/user/2")
+        print(response.json())
         Assertions.assert_json_has_key(response, "username")
         Assertions.assert_json_has_not_key(response, "email")
         Assertions.assert_json_has_not_key(response, "firstName")
@@ -32,6 +31,7 @@ class TestUserGet(BaseCase):
         }
 
         response1 = MyRequests.post("/user/login", data=data)
+        print(response1.json())
         auth_sid = self.get_cookie(response1, "auth_sid")
         token = self.get_header(response1, "x-csrf-token")
         user_id_from_auth_method = self.get_json_value(response1, "user_id")
@@ -39,5 +39,35 @@ class TestUserGet(BaseCase):
         response2 = MyRequests.get(f"/user/{user_id_from_auth_method}",
                                  headers={"x-csrf-token": token}, cookies={"auth_sid": auth_sid})
 
+        print(response2.json())
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+
+
+    """"Тест авторизрванного пользователя для получения данных другого пользователя"""
+
+    @allure.description("This test get details another user")
+    def test_get_another_user_details_auth_as_same_user(self):
+
+        data1 = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data1)
+        print(response1.json())
+
+        data2 = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=data2)
+        print(response2.json())
+        user_id_from_auth_method = self.get_json_value(response2, "id")
+        print(user_id_from_auth_method)
+
+        response3 = MyRequests.get(f"/user/{user_id_from_auth_method}")
+        print(response3.json())
+        expected_fields = ["username", "email", "firstName", "lastName"]
+        Assertions.assert_json_has_key(response3, "username")
+        Assertions.assert_json_has_not_key(response3, "email")
+        Assertions.assert_json_has_not_key(response3, "firstName")
+        Assertions.assert_json_has_not_key(response3, "lastName")
+
